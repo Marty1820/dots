d536a1f7bea57b3a74ba99558d5149d23ffb01bb
Marty1820 <nickm1820@gmail.com>
2024-05-28 20:15:48 +0900

    adding degree F to waybar

--- .config/scripts/weather.sh
+++ .config/scripts/weather.sh
@@ -112,6 +112,6 @@ case $1 in
 	;;
 --waybar)
 	set_icon
-	printf "{\"text\":\"<span foreground=\\\\\"%s\\\\\">%s</span> %s\", \"alt\":\"%s\", \"tooltip\":\"Real feel: %s\"}\n" "$w_hex" "$w_icon" "$w_temp" "$w_stat" "$w_ftemp"
+	printf "{\"text\":\"<span foreground=\\\\\"%s\\\\\">%s</span> %sF\", \"alt\":\"%s\", \"tooltip\":\"Real feel: %s\"}\n" "$w_hex" "$w_icon" "$w_temp" "$w_stat" "$w_ftemp"
 	;;
 esac
