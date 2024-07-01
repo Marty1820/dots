976b6566007be8a12359b3c5c03021cf8c44cef4
Marty1820 <nick.marty@proton.me>
2024-07-01 20:30:32 +0900

    add more icons when paused

--- .config/scripts/screen_brightness_info.sh
+++ .config/scripts/screen_brightness_info.sh
@@ -9,6 +9,9 @@ bright_PID=$(pgrep -f "$bright_script")
 # Status of process STOP/CONT
 bright_status="$(ps -o state= -p "$bright_PID")"
 
+# Get backlight percent
+cur_backlight="$(xbacklight -get)"
+
 bright_toggle() { # Toggle process | STOP/CONT
   if [ "$bright_status" = S ]; then
     kill -STOP "$bright_PID"
@@ -21,7 +24,15 @@ bright_icon() { # Sets brightness icon
   if [ "$bright_status" = S ]; then
     bright_icon="󰃡"
   elif [ "$bright_status" = T ]; then
-    bright_icon="󰳲"
+    if [ "$cur_backlight" -lt "25" ]; then
+      bright_icon="󰃝"
+    elif [ "$cur_backlight" -lt "50" ]; then
+      bright_icon="󰃞"
+    elif [ "$cur_backlight" -lt "75" ]; then
+      bright_icon="󰃟"
+    else
+      bright_icon="󰃠"
+    fi
   fi
   echo "$bright_icon"
 }
