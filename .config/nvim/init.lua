efc1a9e3ad2f3d95d8008f5706a2a20a8e1763b6
Marty1820 <nick.marty@proton.me>
2024-07-13 18:55:12 +0900

    minor formatting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,9 +17,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup("plugins")
+require('lazy').setup('plugins')
 require('lazy').setup({
-  checker = { enabled = true, },
+  checker = { enabled = true },
 })
 
 -- Keybindings for plugins
