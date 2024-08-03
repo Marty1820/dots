dd72c4d280df3227dcc10b117eae840d92544dae
Marty1820 <nick.marty@proton.me>
2024-08-03 22:21:52 +0900

    add comments

--- .config/hypr/hyprpaper.sh
+++ .config/hypr/hyprpaper.sh
@@ -3,9 +3,12 @@
 directory=~/Pictures/wallpapers
 monitor=$(hyprctl monitors | grep Monitor | awk '{print $2}')
 
+# Check if the directory exists and is a directory
 if [ -d "$directory" ]; then
+  # Find a random file in the directory
   random_background=$(find $directory -type f | shuf -n 1)
 
+  # Unload and reload wallpapers
   hyprctl hyprpaper unload all
   hyprctl hyprpaper preload "$random_background"
   hyprctl hyprpaper wallpaper "$monitor, $random_background"
