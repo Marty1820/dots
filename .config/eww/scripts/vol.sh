dc3502a8c81aea75b0b45e17615c43faf877cf66
Marty1820 <nick.marty@proton.me>
2024-08-08 20:11:28 +0900

    fix output error

--- .config/eww/scripts/vol.sh
+++ .config/eww/scripts/vol.sh
@@ -9,6 +9,8 @@ get_volume() {
 get_volume
 
 # Monitor for volume changes
-pactl subscribe | awk '/on sink/ {system("get_volume")}' | while read -r _; do
-  get_volume
+pactl subscribe | while read -r line; do
+  if echo "$line" | grep -q "Event 'change' on sink"; then
+    get_volume
+  fi
 done
