0172436b1e29f3c7265730ab63d3afdae03c892e
Marty1820 <nick.marty@proton.me>
2024-09-04 17:03:09 +0900

    revome text entries from nvim_lsp

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -31,7 +31,12 @@ return {
           ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
+          {
+            name = 'nvim_lsp',
+            entry_filter = function(entry, ctx)
+              return require('cmp.types').lsp.CompletionItemKind[entry:get_kind()] ~= 'Text'
+            end,
+          },
           { name = 'luasnip' }, -- For luasnip users.
         }, {
           { name = 'buffer' },
