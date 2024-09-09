1e4d3b966df0895c96dfafeefbe1078fdb62c2b9
Marty1820 <nick.marty@proton.me>
2024-09-10 07:56:48 +0900

    make closer to default

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -2,15 +2,6 @@ return {
   'MeanderingProgrammer/render-markdown.nvim',
   opts = {
     file_types = { 'markdown', 'norg', 'rmd', 'org' },
-    code = {
-      sign = false,
-      width = 'block',
-      right_pad = 1,
-    },
-    heading = {
-      sign = false,
-      icons = {},
-    },
   },
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
