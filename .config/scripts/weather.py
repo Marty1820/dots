150213c06e133bdc6a8135dad4ac70821c9489d1
Marty1820 <nick.marty@proton.me>
2024-09-16 10:39:37 +0900

    migrating from current & forecast weather to onecall

--- .config/scripts/weather.py
+++ .config/scripts/weather.py
@@ -8,15 +8,16 @@ from datetime import datetime
 
 # Define paths
 home = os.path.expanduser("~")
-cache_dir = os.path.join(home, ".cache", "weather")
-weather_file = os.path.join(cache_dir, "weatherdata.json")
-weather_forecast = os.path.join(cache_dir, "weatherforecast.json")
-aqi_file = os.path.join(cache_dir, "aqidata.json")
 api_file_path = os.path.join(home, ".config", "scripts", "openweatherdata")
+cache_dir = os.path.join(home, ".cache", "weather")
+onecall_file = os.path.join(cache_dir, "onecall.json")
+weather_file = os.path.join(cache_dir, "weatherdata.json")
+aqi_file = os.path.join(cache_dir, "aqidata.json")
 
 # Read API credentials and parameters
 with open(api_file_path, "r") as file:
     api_key, lat, lon = [line.strip() for line in file]
+
 unit = "imperial"  # Available options: 'metric' or 'imperial'
 
 # Ensure cache directory exists
@@ -24,25 +25,31 @@ os.makedirs(cache_dir, exist_ok=True)
 
 
 def fetch_data(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return response.json()
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"Error fetching data: {e}")
+        sys.exit(1)
 
 
 def get_weather_data():
-    base_url = "http://api.openweathermap.org/data/2.5/"
+    base_url = "http://api.openweathermap.org/data/"
     params = f"appid={api_key}&lat={lat}&lon={lon}&units={unit}"
+    excludes = f"&exlude=minutely,hourly"
     urls = {
-        "weather": base_url + "weather?" + params,
-        "forecast": base_url + "forecast/daily?" + params + "&cnt=4",
-        "air_pollution": base_url + "air_pollution?" + params,
+        "onecall": base_url + "3.0/onecall?" + params + excludes,
+        "weather": base_url + "2.5/weather?" + params,
+        "air_pollution": base_url + "2.5/air_pollution?" + params,
     }
+
     data = {key: fetch_data(url) for key, url in urls.items()}
 
+    with open(onecall_file, "w") as f:
+        json.dump(data["onecall"], f, indent=4)
     with open(weather_file, "w") as f:
         json.dump(data["weather"], f, indent=4)
-    with open(weather_forecast, "w") as f:
-        json.dump(data["forecast"], f, indent=4)
     with open(aqi_file, "w") as f:
         json.dump(data["air_pollution"], f, indent=4)
 
@@ -59,47 +66,62 @@ def get_jq_value(data, query):
     for key in keys:
         if isinstance(data, list):
             key = int(key) if key.isdigit() else key
-            data = data[key]
+            if key < len(data):
+                data = data[key]
+            else:
+                return None
         elif isinstance(data, dict):
             key = int(key) if key.isdigit() else key
-            data = data.get(key, {})
+            data = data.get(key, None)
+            if data is None:
+                return None
         else:
             return None
     return data
 
 
-def current_weather():
-    data = load_json(weather_file)
+def onecall_weather(day_index=0):
+    data = load_json(onecall_file)
+
+    # Format the day_index corrrectly in the strings
+    temp_high_query = f".daily.{day_index}.temp.max"
+    temp_low_query = f".daily.{day_index}.temp.min"
+    sunrise_query = f".daily.{day_index}.sunrise"
+    sunset_query = f".daily.{day_index}.sunset"
+
     return {
-        "temp": get_jq_value(data, ".main.temp"),
-        "feels_like": get_jq_value(data, ".main.feels_like"),
-        "status": get_jq_value(data, ".weather.0.main"),
-        "city": get_jq_value(data, ".name"),
-        "humidity": get_jq_value(data, ".main.humidity"),
-        "wind_speed": get_jq_value(data, ".wind.speed"),
-        "clouds": get_jq_value(data, ".clouds.all"),
-        "icon": get_jq_value(data, ".weather.0.icon"),
+        "temp": get_jq_value(data, ".current.temp"),
+        "feels_like": get_jq_value(data, ".current.feels_like"),
+        "status": get_jq_value(data, ".current.weather.0.main"),
+        # "city": get_jq_value(data, f".current."),
+        "humidity": get_jq_value(data, ".current.humidity"),
+        "wind_speed": get_jq_value(data, ".current.wind_speed"),
+        "clouds": get_jq_value(data, ".current.clouds"),
+        "icon": get_jq_value(data, ".current.weather.0.icon"),
+        "temphigh": get_jq_value(data, temp_high_query),
+        "templow": get_jq_value(data, temp_low_query),
+        "sunrise": (
+            datetime.fromtimestamp(get_jq_value(data, sunrise_query)).strftime(
+                "%I:%M %p"
+            )
+            if get_jq_value(data, sunrise_query)
+            else None
+        ),
+        "sunset": (
+            datetime.fromtimestamp(get_jq_value(data, sunset_query)).strftime(
+                "%I:%M %p"
+            )
+            if get_jq_value(data, sunset_query)
+            else None
+        ),
     }
 
 
-def forecast_weather(day_index):
-    data = load_json(weather_forecast)
-    day_data = lambda key: get_jq_value(data, f".list.{day_index}.{key}")
-
-    return (
-        day_data("temp.max"),
-        day_data("temp.min"),
-        (
-            datetime.fromtimestamp(day_data("sunrise")).strftime("%I:%M %p")
-            if day_data("sunrise")
-            else None
-        ),
-        (
-            datetime.fromtimestamp(day_data("sunset")).strftime("%I:%M %p")
-            if day_data("sunset")
-            else None
-        ),
-    )
+def current_weather():
+    data = load_json(weather_file)
+    return {
+        "city": get_jq_value(data, ".name"),
+    }
 
 
 def set_aqi():
@@ -136,7 +158,7 @@ def set_icon():
         "50d": ("󰼰", "#6272a4"),
         "50n": ("󰖑", "#6272a4"),
     }
-    icon_code = current_weather()["icon"]
+    icon_code = onecall_weather()["icon"]
     return icon_map.get(icon_code, ("󰼯", "#ff5555"))
 
 
@@ -156,29 +178,29 @@ def main():
     elif option == "--hex":
         print(set_icon()[1])
     elif option == "--temp":
-        print(current_weather()["temp"])
+        print(onecall_weather()["temp"])
     elif option == "--temphigh":
         if len(sys.argv) < 3:
             print("Index required for --temphigh")
             sys.exit(1)
-        print(forecast_weather(int(sys.argv[2]))[0])
+        print(onecall_weather(int(sys.argv[2]))["temphigh"])
     elif option == "--templow":
         if len(sys.argv) < 3:
             print("Index required for --templow")
             sys.exit(1)
-        print(forecast_weather(int(sys.argv[2]))[1])
+        print(onecall_weather(int(sys.argv[2]))["templow"])
     elif option == "--feel":
-        print(current_weather()["feels_like"])
+        print(onecall_weather()["feels_like"])
     elif option == "--stat":
-        print(current_weather()["status"])
+        print(onecall_weather()["status"])
     elif option == "--city":
         print(current_weather()["city"])
     elif option == "--humid":
-        print(current_weather()["humidity"])
+        print(onecall_weather()["humidity"])
     elif option == "--wind":
-        print(current_weather()["wind_speed"])
+        print(onecall_weather()["wind_speed"])
     elif option == "--clouds":
-        print(current_weather()["clouds"])
+        print(onecall_weather()["clouds"])
     elif option == "--aqi":
         print(set_aqi()[0])
     elif option == "--aqi_color":
@@ -189,12 +211,12 @@ def main():
         if len(sys.argv) < 3:
             print("Index required for --srise")
             sys.exit(1)
-        print(forecast_weather(int(sys.argv[2]))[2])
+        print(onecall_weather(day_index=int(sys.argv[2]))["sunrise"])
     elif option == "--sset":
         if len(sys.argv) < 3:
             print("Index required for --sset")
             sys.exit(1)
-        print(forecast_weather(int(sys.argv[2]))[3])
+        print(onecall_weather(day_index=int(sys.argv[2]))["sunset"])
     else:
         print(
             "Usage: script.py {--getdata|--icon|--temp|--temphigh index|--templow index|--feel|--stat|--city|--humid|--wind|--aqi|--aqi_color|--aqi_icon|--srise|--sset}"
