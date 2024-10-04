b882e7c1da3bbbdecdb10592979fb361acb15ab6
Marty1820 <nick.marty@proton.me>
2024-10-04 15:55:51 +0900

    combined variable declarations

--- .config/eww/scripts/net.sh
+++ .config/eww/scripts/net.sh
@@ -2,12 +2,9 @@
 
 # Function to get WiFi signal and ESSID
 get_wifi_info() {
-  local signal
-  local essid
-
+  local signal essid
   signal=$(nmcli -f in-use,signal dev wifi | awk '/\*/ {print $2}')
   essid=$(nmcli -t -f NAME connection show --active | head -n1 | sed 's/"/\\"/g')
-
   echo "{\"essid\": \"$essid\", \"signal\": \"$signal\"}"
 }
 
