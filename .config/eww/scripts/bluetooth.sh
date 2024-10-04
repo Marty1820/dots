99194c22e43dac211e511e7db54d31cb8d0303ad
Marty1820 <nick.marty@proton.me>
2024-10-04 16:03:20 +0900

    function naming/local variables/reduced redundant commands

--- .config/eww/scripts/bluetooth.sh
+++ .config/eww/scripts/bluetooth.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -eu -o pipefail
 
 # Get Bluetooth status and connected device UUID
@@ -11,7 +10,8 @@ if [ -n "$UUID" ]; then
   DEVICE=$(bluetoothctl info "$UUID")
 fi
 
-_toggle() {
+# Toggle Bluetooth power
+toggle() {
   if [ "$STATUS" = "yes" ]; then
     bluetoothctl power off
   else
@@ -19,79 +19,53 @@ _toggle() {
   fi
 }
 
-_get_device_info() {
+# Get device alias from the info
+get_device_info() {
   echo "$1" | awk -F ': ' '/Alias/ {print $2}'
 }
 
-_get_battery_percent() {
+# Get battery percentage from the info
+get_battery_percent() {
   echo "$1" | awk -F '[()]' '/Battery Percentage/ {print $2}'
 }
 
-_get_icon() {
-  ICON=$(echo "$1" | awk -F ': ' '/Icon/ {print $2}')
+# Get device icon based on its type
+get_icon() {
+  local icon
+  icon=$(echo "$1" | awk -F ': ' '/Icon/ {print $2}')
 
-  case $ICON in
-  audio-headphones|audio-headset)
-    echo "󰥰"
-    ;;
-  input-gaming)
-    echo ""
-    ;;
-  input-keyboard)
-    echo "󰌌"
-    ;;
-  input-mouse)
-    echo "󰦋"
-    ;;
-  input-tablet)
-    echo "󰓷"
-    ;;
-  phone)
-    echo "󰏳"
-    ;;
-  computer|video-display)
-    echo "󰪫"
-    ;;
-  printer)
-    echo "󰐪"
-    ;;
-  scanner)
-    echo "󰚫"
-    ;;
-  camera-photo|camera-video)
-    echo "󰄄"
-    ;;
-  network-wireless)
-    echo "󰂴"
-    ;;
-  audio-card|multimedia-player)
-    echo "󰗾"
-    ;;
-  modem|unknown)
-    echo "󰂱"
-    ;;
+  case $icon in
+  audio-headphones|audio-headset) echo "󰥰" ;;
+  input-gaming) echo "" ;;
+  input-keyboard) echo "󰌌" ;;
+  input-mouse) echo "󰦋" ;;
+  input-tablet) echo "󰓷" ;;
+  phone) echo "󰏳" ;;
+  computer|video-d y) echo "󰪫" ;;
+  pinter) echo "󰐪" ;;
+  scanner) echo "󰚫" ;;
+  camera-photo|camera-video) echo "󰄄" ;;
+  network-wireless) echo "󰂴" ;;
+  audio-card|multimedia-player) echo "󰗾" ;;
+  modem|unknown) echo "󰂱" ;;
   *) echo "" ;;
   esac
 }
 
 # Handle script arguments
 case $1 in
-  --toggle)
-    _toggle
-    ;;
+  --toggle) toggle ;;
   --icon)
-    ICON=$(_get_icon "$DEVICE")
-    if [ -z "$ICON" ]; then
-      ICON=$([ "$STATUS" = "yes" ] && echo "󰂯" || echo "󰂲")
-    fi
+    ICON=$(get_icon "$DEVICE")
+    [ -z "$ICON" ] && ICON=$([ "$STATUS" = "yes" ] && echo "󰂯" || echo "󰂲")
     echo "$ICON"
     ;;
   --bat)
-    BATTERY_PERCENT=$(_get_battery_percent "$DEVICE")
+    BATTERY_PERCENT=$(get_battery_percent "$DEVICE")
     echo "$BATTERY_PERCENT"
     ;;
   --info)
-    INFO=$(_get_device_info "$DEVICE")
+    INFO=$(get_device_info "$DEVICE")
     echo "$INFO"
     ;;
   *)
