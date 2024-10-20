e924e403ca10715c32d8370a128c81489c670bcb
Marty1820 <nick.marty@proton.me>
2024-10-20 14:49:39 +0900

    compacted script and removed duplicate section

--- .config/eww/scripts/revealer.sh
+++ .config/eww/scripts/revealer.sh
@@ -11,16 +11,9 @@ menu_name=$1
 # Get the current value
 current_value=$(eww get "${menu_name}rev")
 
-# Determine the new value for revealer
-if [ "$current_value" = "true" ] || [ -z "$current_value" ]; then
-  new_value="false"
-else
-  new_value="true"
-fi
-
 # Execute commands based on the current value
 if [ "$current_value" = "true" ]; then
-    eww update "${menu_name}rev=$new_value" && sleep 1 && eww open --toggle "$menu_name" &
+    eww update "${menu_name}rev=false" && sleep 1 && eww close "$menu_name" &
 else
-    eww open --toggle "$menu_name" && eww update "${menu_name}rev=$new_value" &
+    eww open "$menu_name" && eww update "${menu_name}rev=true" &
 fi
