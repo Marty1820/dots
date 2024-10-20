5bdce149bb5d23aac23c40421abc94e54e365b67
Marty1820 <nick.marty@proton.me>
2024-10-20 10:47:53 +0900

    eww widgets now slide in and out upon open and close

---
+++ .config/eww/scripts/revealer.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# Check if a menu name was provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <menu_name>"
+  exit 1
+fi
+
+menu_name=$1
+
+# Get the current value
+current_value=$(eww get "${menu_name}rev")
+
+# Determine the new value for revealer
+if [ "$current_value" = "true" ] || [ -z "$current_value" ]; then
+  new_value="false"
+else
+  new_value="true"
+fi
+
+# Execute commands based on the current value
+if [ "$current_value" = "true" ]; then
+    eww update "${menu_name}rev=$new_value" && sleep 1 && eww open --toggle "$menu_name" &
+else
+    eww open --toggle "$menu_name" && eww update "${menu_name}rev=$new_value" &
+fi
