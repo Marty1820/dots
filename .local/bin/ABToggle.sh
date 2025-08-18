663d7d61950ce33fdf5f695aa08c7802da268930
Nick MarT <nick.marty@protom.me>
2025-08-18 17:36:09 +0900

    merge brightness and toggle notification together

--- .local/bin/ABToggle.sh
+++ .local/bin/ABToggle.sh
@@ -4,8 +4,8 @@ service="AB.service"
 
 if systemctl --user is-active --quiet "$service"; then
   systemctl --user stop "$service"
-  notify-send -t 1000 -a 'AB' "Adaptive Brightness off"
+  notify-send -t 1000 -a 'bk-lit' "Adaptive Brightness off"
 else
   systemctl --user start "$service"
-  notify-send -t 1000 -a 'AB' "Adaptive Brightness on"
+  notify-send -t 1000 -a 'bk-lit' "Adaptive Brightness on"
 fi
