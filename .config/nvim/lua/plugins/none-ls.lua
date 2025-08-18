d152e5474bc0bedf05b1c01dc019e12eea1f39e5
Nick MarT <nick.marty@protom.me>
2025-08-18 17:28:13 +0900

    add shellcheck

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "mason.nvim", "nvim-lua/plenary.nvim", "jay-babu/mason-null-ls.nvim" },
   config = function()
     require("mason-null-ls").setup({
-      ensure_installed = { "prettier", "black", "stylua" },
+      ensure_installed = { "prettier", "black", "stylua", "shellcheck" },
     })
     local null_ls = require("null-ls")
 
