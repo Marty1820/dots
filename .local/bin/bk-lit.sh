a45b2a9fe639cf8ec4152546de5e026c8f2f610c
Nick MarT <nick.marty@protom.me>
2025-08-19 17:08:09 +0900

    fix variable splitting

--- .local/bin/bk-lit.sh
+++ .local/bin/bk-lit.sh
@@ -3,4 +3,4 @@
 # Get the brightness level and convert it to a percentage
 brightness=$(brightnessctl info | awk -F '[()%]' '/Current/ {print $2}')
 
-notify-send -t 1000 -a 'bk-lit' -h int:value:$brightness "Brightness: ${brightness}%"
+notify-send -t 1000 -a 'bk-lit' -h int:value:"$brightness" "Brightness: ${brightness}%"
