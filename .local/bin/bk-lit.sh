de0686cc823e14dde53e62c4c5283c35d1f23ea1
Nick MarT <nick.marty@protom.me>
2025-09-14 15:26:58 +0900

    follow man page order&naming/urgency set to low

--- .local/bin/bk-lit.sh
+++ .local/bin/bk-lit.sh
@@ -3,4 +3,4 @@
 # Get the brightness level and convert it to a percentage
 brightness=$(brightnessctl info | awk -F '[()%]' '/Current/ {print $2}')
 
-notify-send -t 1000 -a 'bk-lit' -h int:value:"$brightness" "Brightness: ${brightness}%"
+notify-send --app-name="bk-lit" --urgency low --expire-time=1000 --hint=int:value:"$brightness" "Brightness: ${brightness}%"
--- .local/bin/wp-vol.sh
+++ .local/bin/wp-vol.sh
@@ -3,4 +3,4 @@
 # Get the volume level and convert it to a percentage
 volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d", $2 * 100}')
 
-notify-send -t 1000 -a 'wp-vol' -h int:value:"$volume" "Volume: ${volume}%"
+notify-send --app-name="wp-vol" --urgency low --expire-time=1000 --hint=int:value:"$volume" "Volume: ${volume}%"
