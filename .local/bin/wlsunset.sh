c3574c3bfb443eba685290983ea9fc72ae7003aa
Nick MarT <nick.marty@proton.me>
2026-03-22 18:50:18 +0900

    avoid set/order Lat/Lon doesn't matter/fails cleaning if missing

--- .local/bin/wlsunset.sh
+++ .local/bin/wlsunset.sh
@@ -2,9 +2,17 @@
 
 CONFIG_FILE="$HOME/.local/share/location.toml"
 
-set -- $(awk -F= '
+awk -F= '
   /^\s*LAT/ {gsub(/[" ]/,"",$2); lat=$2}
-  /^\s*LON/ {gsub(/[" ]/,"",$2); lon=$2; print lat, lon; exit}
-' "$CONFIG_FILE")
-
-exec /usr/bin/wlsunset -l "$1" -L "$2"
+  /^\s*LON/ {gsub(/[" ]/,"",$2); lon=$2}
+  END {
+    if (lat && lon)
+      printf "%s %s\n", lat, lon
+    else
+      exit 1
+  }
+' "$CONFIG_FILE" |
+{
+  read -r lat lon || exit 1
+  exec /usr/bin/wlsunset -l "$lat" -L "$lon"
+}
