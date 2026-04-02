b0e3500f666cc3949381aa10a17f9ce494d7ab70
Nick MarT <nick.marty@proton.me>
2026-04-03 07:11:11 +0900

    move vol & bk-lit to just waybar

--- .local/bin/bk-lit.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Get the brightness level and convert it to a percentage
-brightness=$(brightnessctl info | awk -F '[()%]' '/Current/ {print $2}')
-
-notify-send --app-name="bk-lit" --urgency low --expire-time=1000 --hint=int:value:"$brightness" "Brightness: ${brightness}%"
--- .local/bin/wp-vol.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Get the volume level and convert it to a percentage
-volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf "%d", $2 * 100}')
-
-notify-send --app-name="wp-vol" --urgency low --expire-time=1000 --hint=int:value:"$volume" "Volume: ${volume}%"
